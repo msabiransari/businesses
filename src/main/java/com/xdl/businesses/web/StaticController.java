4ba3a496fc15608e1a1da49bbe9859d9ee6b4742
Muhammad Sabir <msabiransari@gmail.com>
2020-10-11 02:51:26 -0400

    WIP

--- src/main/java/com/xdl/businesses/web/StaticController.java
+++ src/main/java/com/xdl/businesses/web/StaticController.java
@@ -1,24 +1,26 @@
 package com.xdl.businesses.web;
 
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @CrossOrigin(origins = { "http://localhost:8080", "https://10xdl.groovepages.com"})
-@Controller
+@RestController
 public class StaticController {
 
   @GetMapping("/app.js")
   public String appJson() {
     StringBuilder builder = new StringBuilder();
 
-    try (Stream<String> stream = Files.lines(Paths.get(new ClassPathResource("/static/app.js").getPath()))) {
-      stream.forEach(builder::append);
+    try (BufferedReader stream = new BufferedReader(new InputStreamReader(new ClassPathResource("/app.js").getInputStream()))) {
+      String str;
+      while((str = stream.readLine()) != null) {
+        builder.append(str);
+      }
     } catch (Exception e) {
       e.printStackTrace();
     }
