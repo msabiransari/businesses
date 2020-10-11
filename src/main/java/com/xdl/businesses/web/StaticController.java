7c3ae1263b8de9b277adf0dee416dad6ee4b303a
Muhammad Sabir <msabiransari@gmail.com>
2020-10-11 02:05:18 -0400

    WIP

--- src/main/java/com/xdl/businesses/web/StaticController.java
+++ src/main/java/com/xdl/businesses/web/StaticController.java
@@ -9,7 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-@CrossOrigin
+@CrossOrigin(origins = { "http://localhost:8080", "https://local-biz.herokuapp.com"})
 @Controller
 public class StaticController {
 
